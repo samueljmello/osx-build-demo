36b5ee5fd1c3b4449ea15543f84d2c5028437f73
Samuel Mello <samueljmello@github.com>
2022-05-19 08:48:43 -0400

    xcode project info

---
+++ app/demo/Shared/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  Shared
+//
+//  Created by Samuel Mello on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
